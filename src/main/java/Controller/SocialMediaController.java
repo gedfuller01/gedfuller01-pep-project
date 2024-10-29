f9bdfd3219591eb8e76c70cccf165e65f5fe7476
Gerald Fuller <gedfuller01@gmail.com>
2024-10-29 19:13:39 +0000

    Register User Completed

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,7 +1,14 @@
 package Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import Model.Account;
+import Service.AccountService;
+import DAO.AccountDAO;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
@@ -9,6 +16,11 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    AccountService accountService;
+
+    public SocialMediaController(){
+        accountService = new AccountService();
+    }
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -17,6 +29,17 @@ public class SocialMediaController {
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
+        app.get("/messages", this::getAllMessages);
+        app.get("/messages/{message_id}", this::getMessageById);
+        app.get("/accounts/{account_id}/messages", this::getAllMessagesByUser);
+
+        app.post("/register", this::userRegistration);
+        app.post("/login", this::userLogin);
+        app.post("/messages", this::createNewMessage);
+
+        app.delete("/messages/{message_id}", this::deleteMessage);
+
+        app.patch("/messages/{message_id}", this::updateMessage);
 
         return app;
     }
@@ -27,6 +50,46 @@ public class SocialMediaController {
      */
     private void exampleHandler(Context context) {
         context.json("sample text");
+    }
+
+    private void getAllMessages(Context context){
+        context.json("filler");
+    }
+
+    private void getMessageById(Context context){
+        context.json("filler");
+    }
+
+    private void getAllMessagesByUser(Context context){
+        context.json("filler");
+    }
+
+    private void userRegistration(Context context) throws JsonProcessingException{
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(context.body(), Account.class);
+        Account addedAccount = accountService.addAccount(account);
+        if(addedAccount != null){
+            context.json(mapper.writeValueAsString(addedAccount));
+        }
+        else{
+            context.status(400);
+        }
+    }
+
+    private void userLogin(Context context){
+        context.json("filler");
+    }
+
+    private void createNewMessage(Context context){
+        context.json("filler");
+    }
+
+    private void deleteMessage(Context context){
+        context.json("filler");
+    }
+
+    private void updateMessage(Context context){
+        context.json("filler");
     }
 
 
---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,36 @@
+package Service;
+
+import Model.Account;
+import DAO.AccountDAO;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class AccountService {
+    private AccountDAO accountDAO;
+
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
+
+    public List<Account> getAllAccounts(){
+        return accountDAO.getAllAccounts();
+    }
+
+    public Account addAccount(Account account){
+        List<Account> accounts = this.getAllAccounts();
+        if(account.getUsername() != "" && account.getPassword().length() >=4){
+            for (int i = 0; i < accounts.size(); i++){
+                if(accounts.get(i).getUsername() != account.getUsername()){
+                    return accountDAO.userRegistration(account);
+                }
+            }
+        }
+        return null;
+        
+    }
+}
